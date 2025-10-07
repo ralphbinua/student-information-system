a2c224b0071a70d60b6d4570c2b6032e8d5d9cb4
Ralph Binua <binua.ralphgabrielb@gmail.com>
2025-10-07 23:15:50 +0800

    test

---
+++ backend/src/models/student-information.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+
+const studentInformationSchema = new mongoose.Schema({
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    dateOfBirth: { type: Date, required: true },
+    age:{ type: Number, required: true },
+},
+{ timestamps: true }
+);
+
+const StudentInformation = mongoose.model('StudentInformation', studentInformationSchema);
+
+export default StudentInformation;
